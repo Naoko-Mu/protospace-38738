832feb25603c8dc2ed4c209d9f46afc90eb90d10
Naoko-Mu <118959113+Naoko-Mu@users.noreply.github.com>
2022-12-16 20:21:05 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,65 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototype = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+
+    if @prototype.save
+      redirect_to root_path(@prototype)
+    else
+      render :new
+    end
+  end
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+  end
+
+  def edit
+
+    @prototype = Prototype.find(params[:id])
+
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+
+    if @prototype.save
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:concept, :catch_copy, :title, :image).merge(user_id: current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
